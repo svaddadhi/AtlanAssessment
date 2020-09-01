fb944e201f9e0d85ff11174ff926802d7b4207e7
Vishal Vaddadhi <vvaddadhi@gmail.com>
2020-09-01 12:44:10 -0700

    created a sample task that runs every minute

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cron = require('node-cron');
+const fs = require('fs');
+
 const app = express();
 
 app.use(express.json({ extended: false }));
 
-app.use('/', (req, res) => {res.send('API Running!')});
+app.use('/', (req, res) => {
+  res.send('API Running!');
+});
 
 const PORT = process.env.PORT || 5000;
+
+cron.schedule('* * * * *', function () {
+  console.log('running a task every minute');
+});
 
 app.listen(PORT, () => console.log(`Listening on port:${PORT}`));
